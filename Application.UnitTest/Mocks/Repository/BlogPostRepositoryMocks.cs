7ab3f83e3402d0f55e0d962e2f131db2f03c8ced
Dawid Florian <dawidflorian99@gmail.com>
2023-09-19 22:44:27 +0200

    Add Tests project

---
+++ Application.UnitTest/Mocks/Repository/BlogPostRepositoryMocks.cs
@@ -0,0 +1,23 @@
+﻿using Application.Contracts.Persistence;
+using Application.Services.Interfaces;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.UnitTest.Mocks.Repository
+{
+    public class BlogPostRepositoryMocks
+    {
+        public static Mock<IBlogPostRepository> GetBlogPostRepository()
+        {
+            var mockBlogPostRepository = new Mock<IBlogPostRepository>();
+
+            //mockBlogPostRepository.Setup(repo => repo.Get)
+
+            return mockBlogPostRepository;
+        }
+    }
+}
---
+++ Application.UnitTest/Tests/BlogPostService/AddBlogPostTests.cs
@@ -0,0 +1,35 @@
+﻿using Application.Contracts.Persistence;
+using Application.Mapper;
+using Application.UnitTest.Mocks.Repository;
+using AutoMapper;
+using Microsoft.Extensions.Configuration;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.UnitTest.Tests.BlogPostServiceTests
+{
+    public class AddBlogPostTests
+    {
+        private readonly IMapper _mapper;
+        private readonly Mock<IBlogPostRepository> _mockBlogPostRepository;
+
+        public AddBlogPostTests()
+        {
+            _mockBlogPostRepository = BlogPostRepositoryMocks.GetBlogPostRepository();
+
+            var configurationProvider = new MapperConfiguration(cfg =>
+                {
+                    cfg.AddProfile<MappingProfile>();
+                }
+            );
+
+            _mapper = configurationProvider.CreateMapper();
+        }
+
+
+    }
+}
